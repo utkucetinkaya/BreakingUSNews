6609e1e1f46138e448ab1cc0ec05f46da0f43a61
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2022-12-12 13:02:04 +0300

    Connecting the cell design with code and populating it with data

---
+++ BreakingUSNews/Scenes/NewsFavoriteList/View/FavoritesCell/FavoriteCell.swift
@@ -0,0 +1,72 @@
+//
+//  FavoriteCell.swift
+//  BreakingUSNews
+//
+//  Created by Utku Çetinkaya on 2.12.2022.
+//
+
+import UIKit
+import SDWebImage
+
+protocol FavoriteCellDelegate {
+    func didTapDeleteButton(model: FavoriteNews.Fetch.ViewModel.Favorites?)
+}
+
+class FavoriteCell: UITableViewCell {
+
+    static var identifier: String {
+        get {
+            "FavoriteCell"
+        }
+    }
+    
+    static func register() -> UINib {
+        UINib(nibName: "FavoriteCell", bundle: nil)
+    }
+    
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var favImageView: UIImageView!
+    @IBOutlet weak var favDateLabel: UILabel!
+    @IBOutlet weak var favTitleLabel: UILabel!
+    @IBOutlet weak var favSourceNameLabel: UILabel!
+    
+    var favorites: FavoriteNews.Fetch.ViewModel.Favorites?
+    var delegate: FavoriteCellDelegate?
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        favImageView.round(15)
+        favImageView.layer.masksToBounds = true
+        backView.round(1)
+        backView.addBorder(color: .secondarySystemGroupedBackground, width: 1)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func deleteButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    func configureFavorite(data: FavoriteNews.Fetch.ViewModel.Favorites, delegate: FavoriteCellDelegate ) {
+        favTitleLabel.text = data.title
+        favDateLabel.text = data.publishedAt
+        favDateLabel.text = Date.formattedDateFromString(dateString: data.publishedAt ?? "")
+        favSourceNameLabel.text = data.name
+        
+        if let image = data.urlToImage {
+            favImageView.sd_setImage(with: URL(string: image ))
+
+        } else {
+            let defaultImage = UIImage(named: "us.news")
+            favImageView.image = defaultImage
+        }
+
+        self.delegate = delegate
+     }
+}
