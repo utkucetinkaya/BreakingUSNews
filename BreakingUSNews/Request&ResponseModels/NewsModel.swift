7bfc218da7ac151fc7661a785864e76cc46d9bc3
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2022-12-12 10:59:11 +0300

    Parameters that need to be sent to the API request, intercepts the response from the API and stores the appropriate data. The data to be shown in the interface is stored in the view model.

---
+++ BreakingUSNews/Request&ResponseModels/NewsModel.swift
@@ -0,0 +1,41 @@
+//
+//  NewsModel.swift
+//  BreakingUSNews
+//
+//  Created by Utku Çetinkaya on 13.11.2022.
+//
+
+import Foundation
+import Alamofire
+
+// MARK: - News
+struct NewsListResponse: Codable {
+    let status: String?
+    let totalResults: Int?
+    let articles: [Article]?
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source?
+    let author: String?
+    let title: String?
+    let articleDescription: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String?
+}
+
