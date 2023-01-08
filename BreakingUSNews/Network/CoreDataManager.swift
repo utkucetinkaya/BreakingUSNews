a67798ed8da211ceadc41f1cefe65f25ba485785
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2023-01-08 23:13:22 +0300

    The news added to the favorites is at the top of the list

--- BreakingUSNews/Network/CoreDataManager.swift
+++ BreakingUSNews/Network/CoreDataManager.swift
@@ -26,16 +26,27 @@ class CoreDataManager {
         let context = appDelegate.persistentContainer.viewContext
         
         let item = FavoriteItem(context: context)
-        
-        
         item.publishedAt = model.publishedAt
         item.name = model.name
         item.title = model.title
         item.urlToImage = model.urlToImage
         item.desc = model.description
         item.content = model.content
-        item.url = model.url
+        item.webUrl = model.url
         
+        let fetchRequest: NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest()
+        let favoriteNewsItems = try? context.fetch(fetchRequest)
+        let newsItemCount = favoriteNewsItems?.count ?? 0
+        
+        if newsItemCount == 0 {
+            item.order = 1
+        } else {
+            let sortDescriptor = NSSortDescriptor(key: "order", ascending: false)
+            fetchRequest.sortDescriptors = [sortDescriptor]
+            let favoriteNewsItems = try? context.fetch(fetchRequest)
+            let lastOrder = favoriteNewsItems?.first?.order ?? 0
+            item.order = lastOrder + 1
+        }
         do {
             try context.save()
             completion(.success(()))
@@ -45,22 +56,21 @@ class CoreDataManager {
     }
 
     func fetchingFavoritesFromDataBase(completion: @escaping(Result<[FavoriteItem], Error>) -> Void) {
-
+        
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-
         let context = appDelegate.persistentContainer.viewContext
-        let request: NSFetchRequest<FavoriteItem>
-        let item = FavoriteItem(context: context)
 
-        request = FavoriteItem.fetchRequest()
-
+        let fetchRequest: NSFetchRequest<FavoriteItem> = FavoriteItem.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "order", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        let favoriteNewsItems = try? context.fetch(fetchRequest)
         do {
-            let favorites = try context.fetch(request)
+            let favorites = try context.fetch(fetchRequest)
             completion(.success(favorites))
 
         }catch { completion(.failure(DatabaseError.failedToFetchData))
-        }
     }
+}
 
     func deleteFavorites(model: FavoriteItem, completion: @escaping (Result<Void, Error>) -> Void) {
 
