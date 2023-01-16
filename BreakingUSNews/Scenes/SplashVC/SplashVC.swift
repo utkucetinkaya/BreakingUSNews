522da167d432b0928517b7dae4c95533a6981d64
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2023-01-16 14:11:27 +0300

    Animated splash screen with lottie with app icon

--- BreakingUSNews/Scenes/SplashVC/SplashVC.swift
+++ BreakingUSNews/Scenes/SplashVC/SplashVC.swift
@@ -6,20 +6,35 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashVC: UIViewController {
 
     var window: UIWindow?
-
+    private var animationView: LottieAnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        animationView = .init(name: "onboarding")
+        animationView!.frame = view.bounds
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 0.5
+        
+        view.addSubview(animationView!)
+        
+        animationView!.play()
+        splashVCConfigure()
+    }
+   
+    func splashVCConfigure(){
         DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            let vc = NewsListViewController(nibName: "NewsListViewController", bundle: nil)
-            let navController = UINavigationController(rootViewController: vc)
-            self.window = UIWindow(frame: UIScreen.main.bounds)
-            self.window?.rootViewController = navController
-            self.window?.makeKeyAndVisible()
+            
+            let storyBoard = UIStoryboard(name: "NewsList", bundle: nil)
+            let destVC: NewsListViewController = storyBoard.instantiateViewController(identifier: "NewsList")
+
+            self.navigationController?.setViewControllers([destVC], animated: true)
         }
     }
 }
